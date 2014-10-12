fc39fea4a30b4fdf005f6b3237e6d6babe5be746
James Reeves <jreeves@weavejester.com>
2014-10-12 16:49:27 +0100

    Add project description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-version-script "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Lein plugin to set the project version from a shell script"
+  :url "https://github.com/weavejester/lein-version-script"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
