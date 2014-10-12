bc49d1277e0ab2e6e09f4dd9e3a4c3cc88dd0c1b
James Reeves <jreeves@weavejester.com>
2014-10-12 16:36:14 +0100

    Call script using bash

--- src/lein_version_script/plugin.clj
+++ src/lein_version_script/plugin.clj
@@ -3,11 +3,8 @@
             [clojure.string :as str]
             [leiningen.core.main :as main]))
 
-(defn- run-script [script]
-  (apply sh (str/split script #"\s+")))
-
 (defn- get-version [script]
-  (let [ret (run-script script)]
+  (let [ret (sh "bash" "-c" script)]
     (when-not (zero? (:exit ret))
       (main/abort "External script in :version-script failed with exit code" (:exit ret)))
     (when (str/blank? (:out ret))
